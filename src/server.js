dc20ac6207404fcac0f335d2cdf845c626051492
bongnteh-romarick-ndzelen <ndzelenromarick@gmail.com>
2025-07-11 07:07:47 -0400

    modifyng manual enrollment logic for admin or instructor to enroll students

--- src/server.js
+++ src/server.js
@@ -73,4 +73,4 @@ app.use(errorHandler);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server started on port http://localhost:${PORT}/api-docs`));
